d7b4fddd2e5fb4d6240127db80c3c70b77f886e4
Trevor Christiansen <trevor@tjc.im>
2020-11-16 12:13:22 -0700

    Fixed prysm failed issue Closes #5, Closes #4

--- build.py
+++ build.py
@@ -47,11 +47,10 @@ def query_yes_no(question, default="yes"):
         choice = input().lower()
         if default is not None and choice == '':
             return valid[default]
-        elif choice in valid:
+        if choice in valid:
             return valid[choice]
-        else:
-            print("Please respond with 'yes' or 'no' "
-                  "(or 'y' or 'n').\n")
+        print("Please respond with 'yes' or 'no' "
+              "(or 'y' or 'n').\n")
 
 
 def parse_feed(feed, entry=0):
